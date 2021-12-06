1c704007d230c48f69c5470fd0e1742347ead517
Chanell Cousin <71571386+NellPrima@users.noreply.github.com>
2021-12-06 04:05:33 -0500

    Add files via upload
    Added additional and fixed files to the rep.

---
+++ dune.js
@@ -0,0 +1,44 @@
+const countdown = () => {
+    const countDate = new Date("October 24, 2021 11:55:00").getTime();
+    const now = new Date().getTime();
+    const gap = now - countDate;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+
+    document.querySelector('.day').innerText = textDay;
+    document.querySelector('.hour').innerText = textHour;
+    document.querySelector('.minute').innerText = textMinute;
+    document.querySelector('.second').innerText = textSecond;
+
+  // if(gap < 1000){
+   // changeBg();
+  // }
+};
+
+setInterval(countdown, 1000);
+
+function changeBg(){
+    const images = [
+        'url("img/img1.jpg")',
+        'url("img/img2.jpg")',
+        'url("img/img4.jpg")',
+        'url("img/img7.jpg")',
+        'url("img/img6.jpg")',
+    ]
+
+    const section = document.querySelector('.coming_soon');
+    const bg = images[Math.floor(Math.random() * images.length)];
+    section.style.backgroundImage = bg;
+};
+
+setInterval(changeBg, 10000);
+
+
